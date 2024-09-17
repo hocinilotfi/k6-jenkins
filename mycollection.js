2125b3a2f6e6b0ab4ed140d933e04f275724eed4
Lotfi Hocini <lotfi.hocini@hotmail.com>
2024-09-17 12:48:17 +0200

    report in mycollection

--- mycollection.js
+++ mycollection.js
@@ -2,6 +2,9 @@
 
 import "./libs/shim/core.js";
 import "./libs/shim/expect.js";
+// This will export to HTML as filename "result.html" AND also stdout using the text summary
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 export let options = { maxRedirects: 4 };
 
@@ -26,4 +29,11 @@ export default function() {
       });
     }
   });
+}
+
+export function handleSummary(data) {
+  return {
+    "result.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
 }
