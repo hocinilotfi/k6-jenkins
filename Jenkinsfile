9c05b2953eb9451757837c5a4142e14627ad6d2b
Lotfi Hocini <lotfi.hocini@hotmail.com>
2024-09-17 12:31:41 +0200

    colcored report

--- Jenkinsfile
+++ Jenkinsfile
@@ -37,7 +37,6 @@ pipeline{
     post { 
         always { 
             archiveArtifacts artifacts: 'summary.html', fingerprint: true
-            echo 'I will always say Hello again!'
 
         }
     }
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+// This will export to HTML as filename "result.html" AND also stdout using the text summary
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 // export const options = {
 //   vus: 10,
 //   duration: '30s',
@@ -10,8 +12,11 @@ export default function () {
   sleep(1);
 }
 
+
+
 export function handleSummary(data) {
   return {
-    "summary.html": htmlReport(data),
+    "result.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
   };
 }
